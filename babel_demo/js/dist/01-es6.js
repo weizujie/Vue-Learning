a6ad5762846a4300ee9c9608377410a02f9b607c
weizujie <byojiaoxianz7@outlook.com>
2020-12-27 14:23:50 +0800

    babel demo

---
+++ babel_demo/js/dist/01-es6.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// es6 代码
+var input = [1, 2, 3];
+input = input.map(function (item) {
+  return item + 1;
+});
+console.log(input);
---
+++ babel_demo/js/es6/01-es6.js
@@ -0,0 +1,4 @@
+// es6 代码
+let input = [1, 2, 3]
+input = input.map(item => item + 1)
+console.log(input)
